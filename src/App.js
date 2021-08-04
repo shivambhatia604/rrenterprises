26f790cebbf431c5c09afde9234d701901bfc790
Shivam B <shivambhatia@outlook.com>
2021-08-05 00:18:05 +0530

    to shiv11 branch

--- src/App.js
+++ src/App.js
@@ -91,7 +91,7 @@ class App extends React.Component {
         </section>
 
         <Brand />
-          <div>Shivam</div>
+          <div>Shivam shiv</div>
         <Footer />
       </div>
     );
