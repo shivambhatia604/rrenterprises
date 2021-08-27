c4dfb005273cbdbccf8b6ca49d51243be9f6b967
Shivam B <shivambhatia@outlook.com>
2021-08-27 22:20:29 +0530

    added form button and form component

--- src/components/sendEnquiryButton/enquiryBtn.component.jsx
+++ src/components/sendEnquiryButton/enquiryBtn.component.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import './enquiry.styles.css';
+import './enquiryBtn.styles.css';
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import { Link } from "react-router-dom";
